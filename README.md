2caa20e2984e4f2ef92252e48939558aec6960df
Deepak_Kumar_s <deepakworkspace014@gmail.com>
2025-10-20 13:49:28 +0530

    Added  calculator with basic operations.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Calculator
 A basic calculator using Java
+
+A simple command-line calculator that lets you perform basic math operations \
+(addition, subtraction, multiplication, and division) 
+continuously without restarting the program.
+
+# How to Run
+
+1- Make sure you have Java installed on your computer \
+2- Save the code in a file called 'Calculator.java' \
+3- Open your terminal or command prompt \
+4- Navigate to the folder where you saved the file \
+5- Compile the program using the command  \
+"javac Calculator.java" \
+6- Run the program using the command \
+"java Calculator"
+
+
+# Features
+
+=> Performs basic math: addition, subtraction, multiplication, division \
+=> Prevents division by zero errors \
+=> Continuous calculations \
+=> Easy to reset or exit\
+=> Simple and user-friendly
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,110 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+
+        boolean continueCalculating = true;
+        double result=0;
+        boolean firstCalculation =true;
+
+        System.out.println("===  Calculator ===");
+        System.out.println("Type 'exit' to quit or 'reset' to start fresh\n");
+
+        //multiple calculations loop
+        while (continueCalculating){
+
+            // For first calculation
+            if (firstCalculation){
+
+                System.out.println("Enter the first number: ");
+                result = sc.nextDouble();
+                System.out.println("Current value: " + result);
+                firstCalculation =false;
+            }
+
+            // Ask for operator
+            System.out.print("\nEnter operation (+, -, *, /) or 'exit'/'reset': ");
+            String input= sc.next();
+
+
+            // Exit condition
+            if (input.equalsIgnoreCase("exit")){
+                System.out.println("\nFinal Result: " + result);
+                System.out.println("Thankyou for using Calculator. GoodBye!");
+                continueCalculating=false;
+                continue;
+            }
+
+            // Reset condition
+            if (input.equalsIgnoreCase("reset")){
+                System.out.println("\nResetting calculator...\n");
+                firstCalculation=true;
+                result=0;
+                continue;
+            }
+
+            // Ask for number
+            System.out.println("enter the number: ");
+            double num = sc.nextDouble();
+
+            // Perform operation based on operator
+            switch (input){
+                case "+" :
+                    result = addition(result , num);
+                    System.out.println("Result: " + result);
+                    break;
+                case "-" :
+                    result = subtract(result , num);
+                    System.out.println("Result: " + result);
+                    break;
+                case "*" :
+                    result = multiply(result , num);
+                    System.out.println("Result: " + result);
+                    break;
+                case "/" :
+                    result = divide(result , num);
+                    System.out.println("Result: " + result);
+                    break;
+                default:
+                    System.out.println("Invalid operator! Please use +, -, *, or /");
+            }
         }
+
+        sc.close();
+
+
+
     }
-}
+
+
+    // add method
+    public static double addition(double a, double b){
+        return a + b;
+    }
+
+
+    // subtract method
+    public static double subtract(double a, double b){
+        return a - b;
+    }
+
+    // multiply method
+    public static double multiply(double a, double b){
+        return a * b;
+    }
+    // divide method
+    public static double divide(double a, double b){
+
+        if(b==0){
+            System.out.println("Error: Cannot divide by zero!");
+            return 0;
+        }
+
+        return a / b;
+    }
+
+
+    }
+
